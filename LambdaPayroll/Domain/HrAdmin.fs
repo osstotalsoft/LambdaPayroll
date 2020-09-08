f15ba9105acdd6b25977400529b9f72f64db2878
Radu Popovici <rpopovici@totalsoft.ro>
2020-09-08 09:29:14 +0300

    fix Value not found in HR DB error

--- LambdaPayroll/Domain/HrAdmin.fs
+++ LambdaPayroll/Domain/HrAdmin.fs
@@ -5,22 +5,28 @@ open NBB.Core.Effects.FSharp
 open LambdaPayroll.Domain
 open NBB.Core.Effects
 
-type LoadSideEffect = {
-    Definition: DbElemDefinition
-    Context: PayrollElemContext
-}
-with interface ISideEffect<Result<obj, string>>
+type LoadSideEffect =
+    { Definition: DbElemDefinition
+      Context: PayrollElemContext }
+    interface ISideEffect<Result<obj, string>>
 
-let load definition (contractId, yearMonth) = (Effect.Of {Definition=definition; Context=(contractId, yearMonth)}) |> Effect.wrap
+let load definition (contractId, yearMonth) =
+    (Effect.Of
+        { Definition = definition
+          Context = (contractId, yearMonth) })
+    |> Effect.wrap
 
 
-let readFromDb<'a> (code:string) : PayrollElem<'a> =
-    let cast (value:'b) = 
-        if typeof<'a> = value.GetType() then Ok (box value :?> 'a) else Error "Invalid elem type"
+let readFromDb<'a> (code: string): PayrollElem<'a> =
+    let cast (value: 'b) =
+        if isNull value then Error "Value not found in HR DB"
+        else if typeof<'a> = value.GetType() then Ok(box value :?> 'a)
+        else Error "Invalid elem type"
 
     fun (contractId, yearMonth) ->
         effect {
             let! elemDefinitionStore = ElemDefinitionStoreRepo.loadCurrent
+
             match ElemDefinitionStore.findElemDefinition elemDefinitionStore (ElemCode code) with
             | Ok (elemDefinition) ->
                 match elemDefinition.Type with
@@ -31,18 +37,19 @@ let readFromDb<'a> (code:string) : PayrollElem<'a> =
             | Error e -> return Error e
         }
 
-type GetOtherEmployeeContractsSideEffect = 
-| GetOtherEmployeeContractsSideEffect of contractId : ContractId 
-with interface ISideEffect<ContractId list>
+type GetOtherEmployeeContractsSideEffect =
+    | GetOtherEmployeeContractsSideEffect of contractId: ContractId
+    interface ISideEffect<ContractId list>
 
-let getOtherEmployeeContracts contractId = (Effect.Of (GetOtherEmployeeContractsSideEffect contractId) ) |> Effect.wrap
-
-
-type GetAllEmployeeContractsSideEffect = 
-| GetAllEmployeeContractsSideEffect of contractId : ContractId 
-with interface ISideEffect<ContractId list>
-
-let getAllEmployeeContracts contractId = (Effect.Of (GetAllEmployeeContractsSideEffect contractId) ) |> Effect.wrap
+let getOtherEmployeeContracts contractId =
+    (Effect.Of(GetOtherEmployeeContractsSideEffect contractId))
+    |> Effect.wrap
 
 
+type GetAllEmployeeContractsSideEffect =
+    | GetAllEmployeeContractsSideEffect of contractId: ContractId
+    interface ISideEffect<ContractId list>
 
+let getAllEmployeeContracts contractId =
+    (Effect.Of(GetAllEmployeeContractsSideEffect contractId))
+    |> Effect.wrap
