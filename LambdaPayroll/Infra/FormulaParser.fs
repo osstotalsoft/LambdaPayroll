2579728214a0fa5a0880225181a174199c0f7985
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2020-11-05 11:22:46 +0200

    Feature/dependency detection without marekers (#6)
    * Find dependencies without markers
    Identify tokens with registered elem codes
    
    * remove duplicate deps
    
    * Refactoring
    
    * Refactoring

--- LambdaPayroll/Infra/FormulaParser.fs
+++ LambdaPayroll/Infra/FormulaParser.fs
@@ -1,17 +1,20 @@
 ﻿namespace LambdaPayroll.Infra
 
 open LambdaPayroll.Application.InfraEffects
+open LambdaPayroll.Domain
 
 module FormulaParser =
     open System.Text.RegularExpressions
     let private pattern =  @"@([a-zA-Z0-9_]+)"
     let private evaluator (m : Match) = m.Groups.[1].Value
 
-    let getDeps (formulaWithTokens) =
-        Regex.Matches(formulaWithTokens, pattern, RegexOptions.Compiled)
+    let getDeps (allCodes: Set<ElemCode>) (formula: string) =
+        Regex.Matches(formula, "([a-zA-Z0-9_]+)", RegexOptions.Compiled)
         |> Seq.cast<Match>
         |> Seq.map evaluator
-        |> Seq.toList
+        |> Seq.where (ElemCode >> allCodes.Contains)
+        |> Set
+        |> Set.toList
 
     let stripDepMarkers (formulaWithTokens) =
         Regex.Replace(formulaWithTokens, pattern, MatchEvaluator evaluator, RegexOptions.Compiled)
@@ -19,5 +22,5 @@ module FormulaParser =
 module FormulaParsingService = 
     open FormulaParsingService
 
-    let getFormulaDeps (GetFormulaDepsSideEffect formula) =
-        FormulaParser.getDeps formula
+    let getFormulaDeps (GetFormulaDepsSideEffect(formula, allCodes)) =
+        FormulaParser.getDeps allCodes formula
